1164a46d9a7a197de4b5191c1ce2185398b9ea38
bedirrhaan <bedirrhaan@gmail.com>
2023-11-04 16:39:35 +0300

    42

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: bcopoglu <bcopoglu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 15:36:45 by bcopoglu          #+#    #+#             */
-/*   Updated: 2023/11/02 14:01:07 by bcopoglu         ###   ########.fr       */
+/*   Updated: 2023/11/04 16:38:00 by bcopoglu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,27 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+int	control(so_game *particles, char *map_name)
+{
+	if (!(map_name_checker(map_name)))
+		return (write (1, "Map Name Error\n", 16), 0);
+	if (!(map_open(map_name, particles)))
+		return (write (1, "Map Open Error\n", 16), 0);
+	if (!(map_checker(particles)))
+		return (write (1, "Map Error\n", 11), 0);
+	if (!(wall_control(particles)))
+		return (write (1, "Map Wall Error\n", 16), 0);
+	if (!(component_control(particles)))
+		return (write (1, "Map Component Error\n", 21), 0);
+	return (1);
+}
+
 int	start_game(char *map_name)
 {
 	so_game	particles;
 	int		i;
 
-	if (!(map_name_checker(map_name)))
-		write (1, "Map Name Error\n", 16);
-	if (!(map_open(map_name, &particles)))
-		write (1, "Map Open Error\n", 16);
-	if (!(map_checker(&particles)))
-		write (1, "Map Error\n", 11);
-	if (!(wall_control(&particles)))
-		write (1, "Map Wall Error\n", 16);
-	if (!(component_control(&particles)))
-		write (1, "Map Component Error\n", 21);
+	control(&particles, map_name);
 	i = 0;
 	if (particles.map)
 	{
